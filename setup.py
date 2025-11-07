6cf02509addb06c34404903762f2b86ef7d17002
Michal Habera <michal.habera@gmail.com>
2025-11-07 14:33:25 +0100

    Update setuptools, split deps

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 setup(
     cffi_modules=["src/eig3x3/_build_cffi.py:ffibuilder"],
     packages=["eig3x3"],
-    package_dir={"": "src"},
-    package_data={"eig3x3": ["c/*.c", "c/*.h"]},
+    package_dir={"eig3x3": "src"},
+    package_data={"eig3x3": ["c/eig3x3.h"]},
 )
